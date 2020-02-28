f15fb4f262ab8f0a5422f17c2a44e4c9af599736
kmhamasaki <khamasak@usc.edu>
2020-02-28 15:23:24 -0800

    trying to fix header

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -180,48 +180,6 @@ const SignIn = props => {
     formState.touched[field] && formState.errors[field] ? true : false;
 
   return (
-    <div className={classes.root}>
-      <Grid
-        className={classes.grid}
-        container
-      >
-        <Grid
-          className={classes.quoteContainer}
-          item
-          lg={5}
-        >
-          <div className={classes.quote}>
-            <div className={classes.quoteInner}>
-              <Typography
-                className={classes.quoteText}
-                variant="h1"
-              >
-                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                they sold out High Life.
-              </Typography>
-              <div className={classes.person}>
-                <Typography
-                  className={classes.name}
-                  variant="body1"
-                >
-                  Takamaru Ayako
-                </Typography>
-                <Typography
-                  className={classes.bio}
-                  variant="body2"
-                >
-                  Manager at inVision
-                </Typography>
-              </div>
-            </div>
-          </div>
-        </Grid>
-        <Grid
-          className={classes.content}
-          item
-          lg={7}
-          xs={12}
-        >
           <div className={classes.content}>
             <div className={classes.contentBody}>
               <form
@@ -295,9 +253,6 @@ const SignIn = props => {
               </form>
             </div>
           </div>
-        </Grid>
-      </Grid>
-    </div>
   );
 };
 
